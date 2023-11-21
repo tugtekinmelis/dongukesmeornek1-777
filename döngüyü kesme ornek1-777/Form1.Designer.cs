f42cc738bd2b068919c43902b91aff7541b53869
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2023-11-21 13:32:05 +0300

    Add files via upload

---
+++ döngüyü kesme ornek1-777/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace döngüyü_kesme_ornek1_777
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtMusteriAd = new System.Windows.Forms.TextBox();
+            this.btnYazdır = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "müşteriler";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "müşteri adı";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Tolga",
+            "Doğa ",
+            "Süleyman",
+            "Eren",
+            "Ramazan",
+            "Melek",
+            "Nesrin",
+            "Beyzanur",
+            "Yezdanur",
+            "Ali",
+            "İlayda"});
+            this.listBox1.Location = new System.Drawing.Point(37, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(167, 303);
+            this.listBox1.TabIndex = 2;
+            // 
+            // txtMusteriAd
+            // 
+            this.txtMusteriAd.Location = new System.Drawing.Point(124, 396);
+            this.txtMusteriAd.Name = "txtMusteriAd";
+            this.txtMusteriAd.Size = new System.Drawing.Size(137, 20);
+            this.txtMusteriAd.TabIndex = 3;
+            // 
+            // btnYazdır
+            // 
+            this.btnYazdır.Location = new System.Drawing.Point(254, 155);
+            this.btnYazdır.Name = "btnYazdır";
+            this.btnYazdır.Size = new System.Drawing.Size(137, 61);
+            this.btnYazdır.TabIndex = 4;
+            this.btnYazdır.Text = "yazdır";
+            this.btnYazdır.UseVisualStyleBackColor = true;
+            this.btnYazdır.Click += new System.EventHandler(this.btnYazdır_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnYazdır);
+            this.Controls.Add(this.txtMusteriAd);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtMusteriAd;
+        private System.Windows.Forms.Button btnYazdır;
+    }
+}
+
---
+++ döngüyü kesme ornek1-777/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace döngüyü_kesme_ornek1_777
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYazdır_Click(object sender, EventArgs e)
+        {
+            string ad = txtMusteriAd.Text;
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+            if (listBox1.Items[i].ToString() == ad)
+
+             MessageBox.Show(listBox1.Items[i].ToString() + "adlı müşteri bulundu!");
+             break;
+            }
+        }
+    }
+}
